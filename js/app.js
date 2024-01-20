b603bd798a663ed093563d39d7aceed8d26bb878
Abdulvahobjon <abdulvahob0714@gmail.com>
2024-01-20 22:27:16 +0500

    js 100 question

--- js/app.js
+++ js/app.js
@@ -81,9 +81,430 @@ let dataQuestion = [
     text:" var o'zgaruvchilarni hoisting qiladi va uni qiymati aniqlangan Undefined bo'ladi. var fuvtion scope bo'ladi va funksiya ichida yaratilgan var (ozgaruvchi) funksiyaning istagan joyida ishlatilishi mumkin.  var da bir xil nomli o'zgaruvchilarni yaratish mumkin. let hoisting bo'ladi lekin uni qiymati anqlanmagan. Let block scope bo'ladi va {}  ichidan tashqariga chiqib keta olmaydi. bir hil nomli o'zgaruvchilarni yaratish mukin emas. "
   },
   {
-    id:15,
+    id:16,
+    title:"Nima uchun sizga <i>JSON</i> kerak ?",
+    text:"Brauzer va server o'rtasida ma'lumot almashishda ma'lumotlar faqat matn bo'lishi mumkin. JSON faqat matn bo'lgani uchun uni serverga va serverdan osongina yuborish va istalgan dasturlash tili tomonidan ma'lumotlar formati sifatida foydalanish mumkin."
+  },
+  {
+    id:17,
+    title:" <i>BOM</i> nima ?",
+    text:"JavaScript BOM, yoki \"Browser Object Model\", brauzer obyektlar modeli, brauzerda interfeysni boshqarish uchun JavaScript obyektlarini taqdim etadi. BOM brauzer va brauzerning xususiyatlariga oid ma'lumotlarni olish, o'zgartirish va boshqa amallarni bajarish uchun ishlatiladi."
+  },
+  {
+    id:18,
     title:" <i>Undefined</i> va <i>Null</i> farqi?",
     text:"Undefined - o'zgaruvchi qiymat berilmagan yoki o'zgaruvchilarning mavjud bo'lmaganligini ko'rsatib beradi. Null - qiymatning yoq ekanligini bildiradi.  O'zgaruvchini qiymatini bo'shatish uchun ham ishlatiladi ."
+  },
+  {
+    id:19,
+    title:" setTimeout dan qanday foydalanish kerak ?",
+    text:"setTimeout() usuli funktsiyani chaqirish yoki belgilangan millisekundlardan keyin ifodani baholash uchun ishlatiladi. "
+  },
+  {
+    id:20,
+    title:"<i>Web Storage</i> nima ? Qanday turdagi storagelar mavjud?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:21,
+    title:"<i>Session storage</i> va <i>Local storage</i>ni farqi nimada?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:22,
+    title:"JavaScript <i>single threaded</i>  yoki <i>multi-threaded</i>?",
+    text:"Hozircha javob berilmagan."
+  }, {
+    id:23,
+    title:"Qaysi yo'llar bilan <i>asinxron</i> kod yozishimiz mumkin?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:24,
+    title:"Qaysi yo'llar bilan <i>asinxron</i> kod yozishimiz mumkin?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:25,
+    title:"<i>Promise</i>larni asosiy statelari qaysilar?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:26,
+    title:"<i>Promise chain</i>ing nima?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:27,
+    title:"null == 0 nimaga teng ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:28,
+    title:"<i>Callback hell</i> nima?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:29,
+    title:"<i>Promise.race</i> va <i>Promise.all</i> metodlarining farqi nimada?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:30,
+    title:"JavaScript <i>compiled</i> yoki <i>interpreted</i> tilmi?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:31,
+    title:"<i>IIFE</i> nima?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:32,
+    title:"<i>Clean Code</i> yozish deganda nimani tushunasiz?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:33,
+    title:"<i>Hoisting</i> nima? <i>Variable hoisting</i> va <i>function hoisting</i> qanday ishlaydi?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:34,
+    title:"<i>Obyekt</i> hususiyatlarini qanday qilib o'zgarmas qilishimiz mumkin?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:35,
+    title:"<i>Const</i> yordamida yasalgan obyektni o'zgartirish imkoni bormi?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:36,
+    title:"<i>O'zgaruvchilar</i> turlari va farqlari haqida gapiring.",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:37,
+    title:"<i>Hoisting</i> nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:38,
+    title:"<i>TDZ</i> (Tempororal dead zone) nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:39,
+    title:"<i>Obyektlar</i> qanday ishlaydi ? <i>Primitive</i> qiymatlardan nima farqi bor ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:40,
+    title:"<i>Garbage collection</i> vazifasi va maqsadi nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:41,
+    title:"<i>this</i> kalit so’zi nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+
+  {
+    id:42,
+    title:"<i>call</i>, <i>apply</i> va <i>bind</i> nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:43,
+    title:"<i>Map</i>, <i>Set</i>, <i>WeakMap</i> va <i>WeakSet</i> nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:44,
+    title:"<i>Type coercion</i> va <i>type conversion</i> farqi nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:45,
+    title:"<i>Operatorlar</i> turlari haqida gapiring.",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:46,
+    title:"<i>Oddiy</i> va <i>chiziqli</i> funksiyalar farqi nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:47,
+    title:"<i>Optional chaining</i> nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:48,
+    title:"Tagged template literals (tagged strings) nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+
+  {
+    id:49,
+    title:"<i>Constructor</i> functions nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:50,
+    title:"Nechta <i>Array metodlarini</i> bilasiz ? <i>Reduce</i> va <i>map</i> farqi va ishlashi qanday",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:51,
+    title:"<i>Destructuring assignment</i> nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:52,
+    title:"<i>Pure</i> va <i>impure</i> funksiya nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:53,
+    title:"<i>First-class functions</i> nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:54,
+    title:"<i>Higher-order functions</i> nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:55,
+    title:"<i>Currying</i> nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:56,
+    title:"<i>Logical Operation</i> haqida aytib bering.",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:57,
+    title:"<i>Ternary operator</i> haqida aytib bering.",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:58,
+    title:"<i>Nullish</i> operator nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:59,
+    title:"<i>for loop</i>  nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:60,
+    title:"<i>Math objecti</i> haqida aytib bering.",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:61,
+    title:"<i>Distructure</i> nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:62,
+    title:"<i>Scope</i> nima?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:63,
+    title:"Nega <i>var</i> bugungi kunda kam ishlatiladi?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:64,
+    title:"<i>setTimeout</i> qanday ishlaydi?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:65,
+    title:"<i>this</i> qaysi holatlarda <i>undefined</i> bo'lib qoladi?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:66,
+    title:"<i>prototype</i> vs <i>proto</i> farqi?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:67,
+    title:"Nimaga <i>curry function</i> kerak?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:68,
+    title:"<i>DOM</i> haqida aytib bering.",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:69,
+    title:"<i>Range</i> nima?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:70,
+    title:"<i>Event loop</i> nima?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:71,    
+    title:"<i>Currying</i> nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:72,
+    title:"<i>Blob</i> nima?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:73,
+    title:"<i>fetch</i> nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:74,
+    title:"<i>LocalStorage</i> haqida aytib bering.",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:75,
+    title:"<i>if else</i> nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:76,
+    title:"<i>Mantiqiy operatorlar</i> haqida aytib bering.",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:77,
+    title:"<i>true === 'true'</i>",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:78,
+    title:"Qaysi <i>string metodi</i>  matn boshida va oxiridagi bo'sh joylarni olib tashlaydi?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:79,
+    title:"Qaysi <i>array</i> metodi massivning <i>oxirgi elementi</i>ni olib tashlaydi va uning qiymatini qaytarad?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:80,
+    title:"<i>1+2+'3'</i>",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:81,
+    title:"<i>true === 'true'</i>",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:82,
+    title:"<i>NaN</i> nima ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:83,
+    title:"<i>Console</i> haqida aytib bering.",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:84,
+    title:"<i>this === window</i>",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:85,
+    title:"Agar <i>getElementById</i> bilan tanlangan element topilmasa, u nima qaytaradi?  ",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:86,
+    title:"Agar <i>querySelectorAll</i> bilan tanlangan element topilsa, u nima qaytaradi?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:87,
+    title:"Agar <i>querySelector</i> bilan tanlangan element topilsa, u nima qaytaradi?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:88,
+    title:"<i>DOM</i> , Qanday qilib elementdan bitta keyingi elementni tanlab olish mumkin?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:89,
+    title:"<i>DOM</i> , Qanday qilib elementdan bitta oldingi elementni tanlab olish mumkin?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:90,
+    title:"Agar <i>getElementsByTagName</i> bilan tanlangan element topilsa, u nima qaytaradi?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:91,
+    title:"Agar <i>getElementsByClassName</i> bilan tanlangan element topilmasa, u nima qaytaradi?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:92,
+    title:"JavaScript dasturlashda  <i>kodning qiyinchilik</i> yuzaga kelishi va boshqarilishi qiyin bo'lgan holat nima deb nomlanadi?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:93,
+    title:"Qaysi javascript metodi <i>Promiseni</i> muvaffaqiyatli yakunlaganida (fulfilled) ishga tushiriladi?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:94,
+    title:"Qaysi javascript metodi <i>Promiseni</i> xato (rejected) holatda ishga tushiriladi?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:95,
+    title:"<i>Intervalli</i> taymerni to'xtatish funktsiyasi qaysi?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:96,
+    title:"Javascriptdagi <i>**</i> operatori vazifasi.",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:97,
+    title:"<i>isNaN('abs_javaScript')</i> ",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:98,
+    title:"<i>100</i> gacha bo'lgan raqamlardan <i>random</i> qiling.",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:99,
+    title:"<i>true + ('true' - 0)</i> ?",
+    text:"Hozircha javob berilmagan."
+  },
+  {
+    id:100,
+    title:"<i>Matn</i> - qanday data type hisoblanadi ?",
+    text:"Hozircha javob berilmagan."
   },
 ]
 let list = document.querySelector(".question-list");
