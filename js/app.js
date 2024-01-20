636636b222364c2ec4f72a6c7409e9d7c8aa020f
Abdulvahobjon <abdulvahob0714@gmail.com>
2024-01-20 14:47:20 +0500

    commid

--- js/app.js
+++ js/app.js
@@ -66,6 +66,20 @@ let dataQuestion = [
     title:" <i>Delete</i> operatoridan nima maqsadda foydalaniladi?",
     text:"JavaScript-da delete operator ob'ektdan xususiyatni olib tashlash uchun ishlatiladi. U massivdan elementni olib tashlash uchun ham ishlatilishi mumkin. Massivlar bilan foydalanilganda, u massivda teshik qoldiradi. Biroq, ob'ektlar bilan foydalanilganda, u belgilangan xususiyatni olib tashlaydi. "
   },
+  {
+    id:13,
+    title:"<i>Object</i> nima ?",
+    text:"Obyectni malumotlarini saqlash uchun ishlatiladi. Objectlar Call stace da reference saqlaydi. U reference bizni u Objectni Heap dagi manzilda turgan qiymatiga olib boradi. Primitive lar Cal staceda value saqlashadi."
+  },
+  {
+    id:14,
+    title:"<i>Hoisting</i> nima ?",
+    text:"Tepaga kotarish degan manoni anglatadi. Agar o'zgatuvchilarda ko'radigan bo'lsak ,  var o'zgaruvchisini yaratilgan joydan tepada chaqiradigan bo'lsak Hoisting bizga uni chaqirib olishimizdan oldinga chiqarib beradi. (var i; ) . Hosting o'zgaruvchini qiymatini emas , nomini tepaga ko'taryabdi.  "
+  },{
+    id:15,
+    title:"<i>let</i> , <i>var</i> , <i>const</i> haqida aytib bering ?",
+    text:" var o'zgaruvchilarni hoisting qiladi va uni qiymati aniqlangan Undefined bo'ladi. var fuvtion scope bo'ladi va funksiya ichida yaratilgan var (ozgaruvchi) funksiyaning istagan joyida ishlatilishi mumkin.  var da bir xil nomli o'zgaruvchilarni yaratish mumkin. let hoisting bo'ladi lekin uni qiymati anqlanmagan. Let block scope bo'ladi va {}  ichidan tashqariga chiqib keta olmaydi. bir hil nomli o'zgaruvchilarni yaratish mukin emas. "
+  },
 ]
 let list = document.querySelector(".question-list");
 
