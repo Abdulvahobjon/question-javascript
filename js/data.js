a61387474196f5562770d8e4ee4f62d5f10934da
Abdulvahobjon <abdulvahob0714@gmail.com>
2024-01-29 22:33:42 +0500

    commid

--- js/data.js
+++ js/data.js
@@ -213,7 +213,7 @@ let dataQuestion = [
   {
     id: 31,
     title: "<i>IIFE</i> nima?",
-    text: "Maqul javobni bilsangiz adminga yuborishingiz mumkin.",
+    text: "IIFE - Immediately Invoked Function Expression yani e'lon qilinishi bilan ishga tushadigan funksiya. Ko'proq global scopedan qochish uchun ishlatiladi. ",
     level: "o'rta",
   },
 
--- js/test.js
+++ js/test.js
@@ -1,28 +1,6 @@
-document.getElementById('submitBtn').addEventListener('click', showResults);
-
-function showResults() {
-  // Javoblar olish uchun kodlar
-  var answers = {
-    'question1': 'Javob 1',
-    'question2': 'Javob 2',
-    'question3': 'Javob 3'
-  };
-
-  var resultList = document.getElementById('answersList');
-  resultList.innerHTML = '';
-
-  for (var question in answers) {
-    var listItem = document.createElement('li');
-    listItem.textContent = document.getElementById(question).textContent + ': ' + answers[question];
-    resultList.appendChild(listItem);
-  }
-
-  document.getElementById('test').style.display = 'none';
-  document.getElementById('results').style.display = 'block';
+let a  = ()=>{
+  console.log('salom');
+  a()
 }
 
-
-
-
-
-
+a()
