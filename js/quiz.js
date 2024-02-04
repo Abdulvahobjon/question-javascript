4fa7bf621158036315a97f8126eaf9c57123a431
Abdulvahobjon <abdulvahob0714@gmail.com>
2024-02-04 13:23:15 +0500

    hatolikalr togrilandi

--- js/quiz.js
+++ js/quiz.js
@@ -7,11 +7,10 @@ let c_text = document.querySelector("#c_text");
 let d_text = document.querySelector("#d_text");
 let quizBtn = document.querySelector("#submit");
 
-let dataNewTen = quizData.map(item=>{
-    let random = Math.floor(Math.random()* quizData.length)
-    return quizData[random]
-  }).slice(0, 10)
-console.log(dataNewTen);
+let uniqueQuizData = [...new Set(quizData)];
+let dataNewTen = uniqueQuizData
+  .sort(() => 0.5 - Math.random())
+  .slice(0, 10);
 
 
 let currentQuiz = 0;
@@ -39,6 +38,7 @@ function deselectAnswere() {
 }
 
 function getSelected() {
+  let answer;
   quizOptionsAnswer.forEach(function (item) {
     if (item.checked) {
       answer = item.id;
@@ -46,6 +46,7 @@ function getSelected() {
   });
   return answer;
 }
+
 
 
 
@@ -62,91 +63,17 @@ quizBtn.addEventListener("click", function () {
     } else {
       setTimeout(function(){
         window.location.href = 'https://www.javascript-savollar.uz/'
-      } , 3000)
+      } , 8000)
       if ((dataNewTen.length / 100) * 90 <= score) {
         quiz.innerHTML = `<div class="quiz-resualt">
         <h2 >Siz ${dataNewTen.length} ta dan  ${score} taga javob topdingiz.</h2> <h1>Eng yuqori daraja 🥇</h1>
         </div>`;
-        function happyFun() {
-          const duration = 15 * 1000,
-            animationEnd = Date.now() + duration,
-            defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
-
-          function randomInRange(min, max) {
-            return Math.random() * (max - min) + min;
-          }
-
-          const interval = setInterval(function () {
-            const timeLeft = animationEnd - Date.now();
-
-            if (timeLeft <= 0) {
-              return clearInterval(interval);
-            }
-
-            const particleCount = 50 * (timeLeft / duration);
-
-            // since particles fall down, start a bit higher than random
-            confetti(
-              Object.assign({}, defaults, {
-                particleCount,
-                origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
-              })
-            );
-            confetti(
-              Object.assign({}, defaults, {
-                particleCount,
-                origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
-              })
-            );
-          }, 250);
-        }
-        happyFun()
+       
       } else if((dataNewTen.length / 100) * 70 <= score){
         quiz.innerHTML = `<div class="quiz-resualt">
         <h2 >Siz ${dataNewTen.length} ta dan  ${score} taga javob topdingiz.</h2> <h1>O'rta  daraja 🥈</h1>
         </div>`;
-        function happyFun() {
-          const count = 200,
-          defaults = {
-            origin: { y: 0.7 },
-          };
-        
-        function fire(particleRatio, opts) {
-          confetti(
-            Object.assign({}, defaults, opts, {
-              particleCount: Math.floor(count * particleRatio),
-            })
-          );
-        }
-        
-        fire(0.25, {
-          spread: 26,
-          startVelocity: 55,
-        });
-        
-        fire(0.2, {
-          spread: 60,
-        });
-        
-        fire(0.35, {
-          spread: 100,
-          decay: 0.91,
-          scalar: 0.8,
-        });
-        
-        fire(0.1, {
-          spread: 120,
-          startVelocity: 25,
-          decay: 0.92,
-          scalar: 1.2,
-        });
-        
-        fire(0.1, {
-          spread: 120,
-          startVelocity: 45,
-        });
-        }
-        happyFun()
+       
       } else if((dataNewTen.length / 100) * 50 <= score){
         confetti({
           particleCount: 100,
